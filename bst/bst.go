6b2e791a98da665ee4e29dd3651a46a5a206c928
Martin Czygan <martin.czygan@gmail.com>
2015-01-02 00:54:42 +0100

    add bst tree search

--- bst/bst.go
+++ bst/bst.go
@@ -2,36 +2,52 @@ package bst
 
 import "fmt"
 
-type Node struct {
-	key   *int
-	left  *Node
-	right *Node
+type Tree struct {
+	Key   *int
+	Value interface{}
+	Left  *Tree
+	Right *Tree
 }
 
-func New() *Node {
-	return &Node{}
+func New() *Tree {
+	return &Tree{}
 }
 
-func (n *Node) Insert(key int) {
-	if n.key == nil {
-		n.key = &key
-		n.left = New()
-		n.right = New()
+func (t *Tree) Insert(key int, value interface{}) {
+	if t.Key == nil {
+		t.Key = &key
+		t.Value = value
+		t.Left = New()
+		t.Right = New()
 	} else {
-		if key == *n.key {
+		if key == *t.Key {
 			return
 		}
-		if key < *n.key {
-			n.left.Insert(key)
+		if key < *t.Key {
+			t.Left.Insert(key, value)
 		} else {
-			n.right.Insert(key)
+			t.Right.Insert(key, value)
 		}
 	}
 }
 
-func (n *Node) String() string {
-	if n.key != nil {
-		return fmt.Sprintf("(%d %s %s)", *n.key, n.left, n.right)
+func (t *Tree) Search(key int) *Tree {
+	if t.Key == nil {
+		return nil
+	}
+	if *t.Key == key {
+		return t
+	}
+	if key < *t.Key {
+		return t.Left.Search(key)
+	} else {
+		return t.Right.Search(key)
+	}
+}
+
+func (t *Tree) String() string {
+	if t.Key != nil {
+		return fmt.Sprintf("(%d %s %s)", *t.Key, t.Left, t.Right)
 	} else {
 		return "()"
 	}
--- examples/bst.go
+++ examples/bst.go
@@ -2,22 +2,26 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
 
 	"github.com/miku/arboretum/bst"
 )
 
-func randInt(min int, max int) int {
-	return min + rand.Intn(max-min)
-}
-
 func main() {
-	rand.Seed(time.Now().UTC().UnixNano())
 	tree := bst.New()
-	for i := 0; i < 5; i++ {
-		tree.Insert(randInt(0, 100))
-	}
-	fmt.Printf("%+v\n", tree)
-	// e.g. (75 (22 (5 () ()) ()) (78 () (85 () ())))
+
+	// Insertion
+	tree.Insert(10, "ten")
+	tree.Insert(20, "twenty")
+	tree.Insert(30, "thirty")
+	tree.Insert(5, "five")
+	tree.Insert(15, "fiveteen")
+	fmt.Printf("%+v\n", tree) // (10 (5 () ()) (20 (15 () ()) (30 () ())))
+
+	// Search
+	var t *bst.Tree
+	t = tree.Search(8)
+	fmt.Println(t) // nil
+
+	t = tree.Search(20)
+	fmt.Println(t.Value) // twenty
 }
