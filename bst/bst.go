95c395a7ce336260ab25c78a0cfa918f90321715
Martin Czygan <martin.czygan@gmail.com>
2015-01-03 23:43:23 +0100

    add documentation

--- bst/bst.go
+++ bst/bst.go
@@ -1,7 +1,10 @@
+// bst, a simple binary search tree implementation
 package bst
 
 import "fmt"
 
+// Tree holds a node in a binary tree. An empty node has a nil key,
+// a leaf node has nil left and right trees.
 type Tree struct {
 	Key   *int
 	Value interface{}
@@ -9,10 +12,12 @@ type Tree struct {
 	Right *Tree
 }
 
+// New returns an empty tree
 func New() *Tree {
 	return &Tree{}
 }
 
+// Insert inserts key and value only if key does not exist yet
 func (t *Tree) Insert(key int, value interface{}) {
 	if t.Key == nil {
 		t.Key = &key
@@ -31,6 +36,7 @@ func (t *Tree) Insert(key int, value interface{}) {
 	}
 }
 
+// Search returns the Tree node if the key is found, nil otherwise
 func (t *Tree) Search(key int) *Tree {
 	if t.Key == nil {
 		return nil
@@ -55,27 +61,29 @@ func (t *Tree) leftmostLeaf() *Tree {
 	return t
 }
 
+// Delete remove the node with the given key
 func (t *Tree) Delete(key int) {
 	if t.Key == nil {
 		return
 	}
 	if key == *t.Key {
-		// Leaf
+		// leaf
 		if t.Left.Key == nil && t.Right.Key == nil {
 			t.Key = nil
 		}
-		// Single Child
+		// single left child
 		if t.Left.Key != nil && t.Right.Key == nil {
 			t.Key = t.Left.Key
 			t.Value = t.Left.Value
 			t.Left = New()
 		}
+		// single right child
 		if t.Left.Key == nil && t.Right.Key != nil {
 			t.Key = t.Right.Key
 			t.Value = t.Right.Value
 			t.Right = New()
 		}
-		// Two Children
+		// two children
 		if t.Left.Key != nil && t.Right.Key != nil {
 			successor := t.Right.leftmostLeaf()
 			t.Key = successor.Key
@@ -90,6 +98,7 @@ func (t *Tree) Delete(key int) {
 	}
 }
 
+// String renders the tree as abbreviated s-expressions
 func (t *Tree) String() string {
 	if t.Key != nil {
 		return fmt.Sprintf("(%d %s %s)", *t.Key, t.Left, t.Right)
